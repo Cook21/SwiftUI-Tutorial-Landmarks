7e5528225fd27dabe32b0b7029ef222c63afa275
Cook21 <null>
2022-01-12 15:17:08 +0800

    Initial Commit

---
+++ SwiftUI-Tutorial-Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Tutorial-Landmarks
+//
+//  Created by Larry Xia on 2022/1/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-Tutorial-Landmarks/SwiftUI_Tutorial_LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Tutorial_LandmarksApp.swift
+//  SwiftUI-Tutorial-Landmarks
+//
+//  Created by Larry Xia on 2022/1/12.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Tutorial_LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
