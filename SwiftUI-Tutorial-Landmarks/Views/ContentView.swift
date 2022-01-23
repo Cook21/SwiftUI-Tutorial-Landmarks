7df30fc443c1ed5d9a7e763719497e19b5995f77
Cook21 <null>
2022-01-23 19:40:33 +0800

    3.1 Composing Complex Interfaces

--- SwiftUI-Tutorial-Landmarks/Views/ContentView.swift
+++ SwiftUI-Tutorial-Landmarks/Views/ContentView.swift
@@ -8,8 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selection: Tab = .featured
+    enum Tab {
+        case featured
+        case list
+    }
     var body: some View {
-        LandmarkList()
+        //引用
+        TabView(selection: $selection) {
+            CategoryHome()
+                .tabItem {
+                    Label("Featured", systemImage: "star")
+                }
+            //对应selection是什么值的时候该标签被选中
+                .tag(Tab.featured)
+            LandmarkList()
+                .tabItem {
+                    Label("List", systemImage: "list.bullet")
+                }
+                .tag(Tab.list)
+        }
     }
     
 }
